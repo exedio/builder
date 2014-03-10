b6ca6d59d0bc9c9c7a955a3182ebd7df1c557d28
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-03-10 18:02:43 +0100

    test fallback

--- testsrc/com/exedio/cope/builder/test/BuilderSimpleTest.java
+++ testsrc/com/exedio/cope/builder/test/BuilderSimpleTest.java
@@ -7,9 +7,27 @@ import org.junit.Test;
 public class BuilderSimpleTest extends MainTest
 {
 	@Test
-	public void simple()
+	public void mandatoryExplicit()
 	{
-		final SimpleItem i = new SimpleItemBuilder().integer(88).build();
-		assertEquals(88, i.getInteger());
+		final SimpleItem i = new SimpleItemBuilder().integerMandatory(88).build();
+		assertEquals(88, i.getIntegerMandatory());
+	}
+	@Test
+	public void mandatoryFallback()
+	{
+		final SimpleItem i = new SimpleItemBuilder().build();
+		assertEquals(777777, i.getIntegerMandatory());
+	}
+	@Test
+	public void optionalExplicit()
+	{
+		final SimpleItem i = new SimpleItemBuilder().integerOptional(88).build();
+		assertEquals(Integer.valueOf(88), i.getIntegerOptional());
+	}
+	@Test
+	public void optionalFallback()
+	{
+		final SimpleItem i = new SimpleItemBuilder().build();
+		assertEquals(null, i.getIntegerOptional());
 	}
 }
--- testsrc/com/exedio/cope/builder/test/SimpleItemBuilder.java
+++ testsrc/com/exedio/cope/builder/test/SimpleItemBuilder.java
@@ -3,4 +3,10 @@ package com.exedio.cope.builder.test;
 
 public class SimpleItemBuilder extends GeneratedSimpleItemBuilder<SimpleItemBuilder>
 {
+	@Override
+	public SimpleItem build()
+	{
+		fallback(SimpleItem.integerMandatory, 777777);
+		return super.build();
+	}
 }
