731a9e09381bf9eed40d2440d6a4e1e921f2e7c0
Ralf Wiebicke <ralf.wiebicke@exedio.com>
2014-03-10 17:18:22 +0100

    add BuilderSimpleTest

---
+++ testsrc/com/exedio/cope/builder/test/BuilderSimpleTest.java
@@ -0,0 +1,14 @@
+package com.exedio.cope.builder.test;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class BuilderSimpleTest
+{
+	@Test
+	public void test()
+	{
+		assertTrue( true );
+	}
+}
