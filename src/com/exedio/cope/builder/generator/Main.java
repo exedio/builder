8615b65e92d5ad9f89be7e9cba1851a724d751a3
Martin Heinzerling <martin.heinzerling@exedio.com>
2014-03-13 09:49:39 +0100

    generate also initial concrete builders

--- src/com/exedio/cope/builder/generator/Main.java
+++ src/com/exedio/cope/builder/generator/Main.java
@@ -1,7 +1,5 @@
 package com.exedio.cope.builder.generator;
 
-import java.io.FileNotFoundException;
-
 import com.exedio.cope.Feature;
 import com.exedio.cope.FunctionField;
 import com.exedio.cope.Model;
@@ -23,6 +21,7 @@ import com.exedio.cope.pattern.Range;
 import com.exedio.cope.pattern.RangeField;
 import com.exedio.cope.pattern.SetField;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
@@ -126,6 +125,7 @@ final class Main
 			throw new HumanReadableException("expected directory: " + dir.getAbsolutePath());
 
 		writeGenerated( type, clazz, packageName, simpleClassName, dir );
+		writeConcrete( type, clazz, packageName, simpleClassName, dir );
 		progress.incrementAndGet();
 	}
 
@@ -142,6 +142,27 @@ final class Main
 		try
 		{
 			write(type, packageName, simpleClassName, writer);
+		}
+		finally
+		{
+			writer.close();
+		}
+	}
+
+	private static void writeConcrete( final MyType type, final Class< ? > clazz, final String packageName, final String simpleClassName, final File dir )
+			throws FileNotFoundException, IOException
+	{
+
+		final File file = new File(dir, (type.enableCommonBuilder()?"Common":"") + simpleClassName + "Builder.java");
+
+		if( file.exists())
+			return;
+
+		final CharsetEncoder encoder = Charset.forName("US-ASCII").newEncoder(); // TODO customizable
+		final OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file), encoder);
+		try
+		{
+			writeConcrete(type, packageName, simpleClassName, writer);
 		}
 		finally
 		{
@@ -384,6 +405,94 @@ final class Main
 		writer.write("}");
 		writer.write(newLine);
 	}
+
+
+	private static final void writeConcrete(
+			final MyType type,
+			final String packageName,
+			final String simpleClassName,
+			final OutputStreamWriter writer)
+		throws IOException
+	{
+		final String newLine = System.getProperty("line.separator");
+
+		writer.write("package ");
+		writer.write(packageName);
+		writer.write(';');
+		writer.write(newLine);
+		writer.write(newLine);
+
+		if (type.enableCommonBuilder())
+		{
+			writer.write("import com.exedio.cope.Type;");
+			writer.write(newLine);
+			writer.write(newLine);
+		}
+
+		writer.write("public ");
+		if (type.enableCommonBuilder()) writer.write("abstract ");
+		writer.write("class ");
+
+		if (type.enableCommonBuilder()) writer.write("Common");
+		writer.write(simpleClassName);
+		writer.write("Builder");
+
+		if (type.enableCommonBuilder())
+		{
+
+			writer.write("<I extends "+simpleClassName+", B extends Common"+simpleClassName+"Builder<?,?>>");
+			writer.write(newLine);
+			writer.write("\textends ");
+			writer.write("Generated"+simpleClassName+"Builder<I,B>");
+		}
+		else
+		{
+			writer.write(" extends ");
+			writer.write("Generated"+simpleClassName+"Builder<"+simpleClassName+"Builder>");
+		}
+
+
+		writer.write(newLine);
+
+		writer.write("{");
+		writer.write(newLine);
+
+		if (type.enableCommonBuilder())
+		{
+			writer.write("\tprotected Common"+simpleClassName+"Builder(final Type<I> type)");
+			writer.write(newLine);
+			writer.write("\t{");
+			writer.write(newLine);
+			writer.write("\t\tsuper(type);");
+			writer.write(newLine);
+			writer.write("\t}");
+			writer.write(newLine);
+			writer.write(newLine);
+		}
+
+		writer.write("\t@Override");
+		writer.write(newLine);
+		if (type.enableCommonBuilder())
+		{
+			writer.write("\tpublic I build()");
+		}
+		else
+		{
+			writer.write("\tpublic "+simpleClassName+" build()");
+		}
+
+		writer.write(newLine);
+		writer.write("\t{");
+		writer.write(newLine);
+		writer.write("\t\treturn super.build();");
+		writer.write(newLine);
+		writer.write("\t}");
+
+		writer.write(newLine);
+		writer.write("}");
+		writer.write(newLine);
+	}
+
 
 	// TODO should be part of the framework
 	private static <K extends Enum<K>,V> Class<V> getValueClass(final EnumMapField<K,V> field)
